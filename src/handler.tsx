751e818cc1a5440cb9b79e69b7b20bb636697b4e
devcxl <64475363+devcxl@users.noreply.github.com>
2025-05-27 03:23:15 +0800

    install commit

---
+++ src/handler.tsx
@@ -0,0 +1,35 @@
+// src/handler.ts
+import { OGTemplate } from './template';
+import satori from 'satori';
+export async function handleRequest(request: Request, env: Env): Promise<Response> {
+	const { searchParams } = new URL(request.url);
+	const addon = searchParams.get('addon') || 'fuckoffcsdn';
+	const targetUrl = `https://addons.mozilla.org/api/v5/addons/addon/${addon}`;
+	const addonData: any = await (await fetch(targetUrl)).json();
+	// 从 R2 读取字体
+	const fontObject = await env.ASSETS.get('fonts/NotoSansSC/NotoSansSC-Regular.ttf');
+	if (!fontObject) {
+		return new Response('Font not found in R2', { status: 404 });
+	}
+
+	const fontData = await fontObject.arrayBuffer();
+
+	const svg = await satori(<OGTemplate title={addonData['name']['zh-CN']} iconUrl={addonData.icon_url} description={addonData['summary']['zh-CN']} />, {
+		width: 720,
+		fonts: [
+			{
+				name: 'NotoSansSC',
+				data: fontData,
+				weight: 400,
+				style: 'normal',
+			},
+		],
+	});
+
+	return new Response(svg, {
+		headers: {
+			'Content-Type': 'image/svg+xml',
+			'Cache-Control': 'public, max-age=3600',
+		},
+	});
+}
---
+++ src/template.tsx
@@ -0,0 +1,50 @@
+export function OGTemplate({ title, iconUrl, description }: { title: string; iconUrl: string; description: string }) {
+	return (
+		<div
+			style={{
+				width:'720px',
+				display: 'flex',
+				flexDirection: 'row',
+				alignItems: 'flex-start',
+				padding: '12px',
+				backgroundColor: '#fff',
+				border: '1px solid #e5e7eb',
+			}}
+		>
+			{/* 图标 */}
+			<img
+				src={iconUrl}
+				alt="icon"
+				width={48}
+				height={48}
+				style={{
+					borderRadius: '8px',
+					marginRight: '12px',
+					flexShrink: 0,
+				}}
+			/>
+
+			{/* 文本内容 */}
+			<div style={{ display: 'flex', flexDirection: 'column' }}>
+				<span
+					style={{
+						fontSize: '18px',
+						fontWeight: '600',
+						color: '#111827',
+						marginBottom: '4px',
+					}}
+				>
+					{title}
+				</span>
+				<span
+					style={{
+						fontSize: '16px',
+						color: '#6b7280',
+					}}
+				>
+					{description}
+				</span>
+			</div>
+		</div>
+	);
+}
